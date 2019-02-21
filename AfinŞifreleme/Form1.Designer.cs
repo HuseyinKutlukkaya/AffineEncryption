dcfa96795bffcc92f4516c115ab65d359696b038
Huseyin Kutlukkaya <huseyinkutlukkaya@gmail.com>
2019-02-21 23:59:49 +0300

    Proje Eklendi

---
+++ AfinŞifreleme/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace AfinŞifreleme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(29, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(523, 197);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(29, 256);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(523, 197);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(33, 484);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(523, 197);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(603, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(603, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.label1.Location = new System.Drawing.Point(552, 463);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 29);
+            this.label1.TabIndex = 5;
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(744, 44);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(75, 23);
+            this.btnBasla.TabIndex = 6;
+            this.btnBasla.Text = "BAŞLA";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(562, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "a=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(562, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "b=";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Şifrelenicek metin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Şifrelenmiş";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 463);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Çözülmüş metin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 718);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Doğrusal Şifreleme Hüseyin Kutlukkaya";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ AfinŞifreleme/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AfinŞifreleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList sozluk = new ArrayList();
+        ArrayList alfabe = new ArrayList()
+{"A","B", "C", "Ç", "D","E", "F", "G","Ğ", "H","I", "İ", "J","K","L","M", "N", "O", "Ö", "P", "R", "S", "Ş", "T", "U", "Ü","V", "Y","Z",
+            "a","b", "c", "ç", "d","e","2", "f", "g","1","ğ", "h","ı","3", "i","0", "j","k","l","m", "n", "o", "ö", "p", "r", "s", "ş", "t", "u", "ü","v", "y","z"
+            ,"4","5","6","7","8","9"};
+        ArrayList sayilar = new ArrayList()
+        {"0","1","2","3","4","5","6","7","8","9"};
+        String text = "";
+        int a = 0, b = 0;
+        List<int> Alar = new List<int>();
+        List<int[]> ab = new List<int[]>();
+        ArrayList temp = new ArrayList();
+        List<int[]> SozlukIndex = new List<int[]>();
+
+        List<int> Puanlar = new List<int>();
+        List<int> Maxlar = new List<int>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.ShowDialog();
+            FileStream akis;
+
+            string Yol =file.FileName;
+            akis = new FileStream(Yol, FileMode.Open, FileAccess.Read);
+            StreamReader Okuma = new StreamReader(akis, Encoding.GetEncoding("iso-8859-9"), false);
+
+
+            var satirlar = Okuma.ReadToEnd().Split('\n');
+            foreach (var satır in satirlar)
+            {
+
+
+
+                if (!satır.Contains(" ") && alfabe.Contains(satır.Substring(0, 1).ToUpper()) && !satır.Contains("â") && !satır.Contains("'") && !satır.Contains("î"))
+                {
+                    sozluk.Add(satır.Replace("\r", ""));
+                }
+
+            }
+
+            for (int i = 0; i < 29; i++)
+            {
+
+                SozlukIndex.Add(new int[2]);
+
+
+            }
+            SozlukIndex[0][0] = 0;
+            SozlukIndex[28][1] = sozluk.Count - 1;
+            string karakter = "a";
+            for (int i = 0; i < sozluk.Count; i++)
+            {
+                if (karakter.ToLower() != sozluk[i].ToString().ToLower().Substring(0, 1))
+                {
+                    SozlukIndex[alfabe.IndexOf(karakter.ToUpper())][1] = i - 1;
+                    if (alfabe.IndexOf(karakter.ToUpper()) + 1 < 29)
+                    {
+                        SozlukIndex[alfabe.IndexOf(karakter.ToUpper()) + 1][0] = i;
+
+                    }
+                    karakter = sozluk[i].ToString().Substring(0, 1);
+                }
+
+
+            }
+            AdeğerleriniUret();
+        }
+        string Sifrele(string kelime, int a, int b)
+        {
+            string yeniText = "";
+
+            char[] dizi = kelime.ToCharArray();
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (alfabe.Contains(dizi[i].ToString()))
+                {
+                    int index = alfabe.IndexOf(dizi[i].ToString());
+                    int ip = ((a * index) + b) % alfabe.Count;
+
+                    yeniText += alfabe[ip].ToString();
+                }
+                else
+                    yeniText += dizi[i].ToString();
+
+
+
+            }
+            return yeniText;
+        }
+        String Desifrele(string kelime, int a, int b)
+        {
+            int y = 0;
+            string ex = "";
+            char[] dizi = kelime.ToCharArray();
+            int tersno = 0;
+            y = alfabe.Count;
+            while (true)
+            {
+                if ((y + 1) % a == 0)
+                {
+                    tersno = (y + 1) / a;
+                    y = 0;
+
+                    break;
+                }
+                else
+                {
+                    y += alfabe.Count;
+                }
+
+            }
+
+
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (alfabe.Contains(dizi[i].ToString()))
+                {
+                    int ic = alfabe.IndexOf(dizi[i].ToString());
+                    y = (tersno * (ic - b));
+                    while (y < 0)
+                    {
+                        y += alfabe.Count;
+                    }
+                    y %= alfabe.Count;
+
+                    ex += alfabe[y];
+
+                }
+                else
+                    ex += dizi[i].ToString();
+
+            }
+            return ex;
+        }
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            try
+            {
+                a = Convert.ToInt32(textBox1.Text);
+                if (!Alar.Contains(a))
+                {
+                    MessageBox.Show("a değeri Alfabedeki harf sayisi ile aralarında asal olmalıdır ve  küçük olmalıdır", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                b = Convert.ToInt32(textBox2.Text);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("A ve B değerlerini kontrol ediniz", "HATA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            Puanlar.Clear();
+            temp.Clear();
+            ab.Clear();
+            text = richTextBox1.Text;
+            Maxlar.Clear();
+            temp.AddRange(text.Split(' '));
+            temp.Remove("");
+            for (int i = 0; i < temp.Count; i++)
+            {
+                if (temp[i].ToString() != "")
+                {
+                    richTextBox2.Text += Sifrele(temp[i].ToString(), a, b) + " ";
+                }
+            }
+
+            temp.Clear();
+            temp.AddRange(richTextBox2.Text.Split(' '));
+            temp.Remove("");
+
+            AdeğerleriniUret();
+            for (int i = 0; i < temp.Count; i++)
+            {
+                Maxlar.Add(0);
+            }
+
+            for (int i = 0; i < Alar.Count; i++)
+            {
+
+                for (int j = 0; j < alfabe.Count; j++)
+                {
+                    int ensoneklenen = 0;
+                    int skor = 0;
+                    Puanlar.Add(0);
+                    ab.Add(new int[2] { Alar[i], j });
+                    for (int k = 0; k < temp.Count; k++)
+                    {
+                        skor += temp[k].ToString().Length;
+                        string kelime = Desifrele(temp[k].ToString(), Alar[i], j);
+
+                        string ilkHarf = kelime.Substring(0, 1);
+                        bool a = true;
+                        for (int c = 0; c < kelime.Length; c++)
+                        {
+                            if (sayilar.Contains(kelime[c].ToString()))
+                            {
+                                a = false;
+                            }
+                        }
+                        if (k>=3&&Puanlar[Puanlar.Count-1]<skor/2)
+                        {
+                            break;
+                        }
+                        int dogruSayisi = 0;
+                       if(alfabe.Contains(ilkHarf))
+                        if (!sayilar.Contains( ilkHarf)&&a)
+                        {
+
+                            if (Maxlar[k] != -1)
+                                for (int l = SozlukIndex[alfabe.IndexOf(ilkHarf.ToUpper())][0]; l <= SozlukIndex[alfabe.IndexOf(ilkHarf.ToUpper())][1]; l++)
+                                {
+                                    if (sozluk[l].ToString().ToLower() == kelime.ToLower())
+                                    {
+                                        Puanlar[Puanlar.Count - 1] += kelime.Length;
+                                        Maxlar[k] = -1;
+                                            dogruSayisi++;
+                                            if (dogruSayisi>2)
+                                            {
+                                                EkranaYaz();
+                                                return;
+                                            }
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        for (int m = 1; m < sozluk[l].ToString().Length && m < kelime.ToString().Length; m++)
+                                        {
+
+                                            if (Maxlar[k] != 0 && (Maxlar[k] < sozluk[l].ToString().Length && Maxlar[k] < kelime.ToString().Length) && m == 0)
+                                                if (sozluk[l].ToString().Substring(0, Maxlar[k]).ToLower() != kelime.Substring(0, Maxlar[k]).ToLower())
+                                                    break;
+                                                else
+                                         {
+                                                    m = Maxlar[k];
+                                                }
+
+                                            if (sozluk[l].ToString().ToLower()[m] != kelime.ToLower()[m])
+                                            {
+                                                Maxlar[k] = m;
+                                                Puanlar[Puanlar.Count - 1] -= ensoneklenen;
+                                                Puanlar[Puanlar.Count - 1] += m;
+                                                    if (m>=kelime.Length/2)
+                                                    {
+                                                        if (dogruSayisi > 2)
+                                                        {
+                                                            dogruSayisi++;
+
+                                                            EkranaYaz();
+                                                            return;
+                                                        }
+                                                    }
+                                                ensoneklenen = m;
+                                                break;
+                                            }
+
+
+                                        }
+
+
+
+                                    }
+
+                                }
+                        }
+
+                    }
+                }
+            }
+
+
+            EkranaYaz();
+         
+
+        }
+        void EkranaYaz()
+        {
+            int max = 0;
+            for (int i = 0; i < Puanlar.Count; i++)
+            {
+                if (Puanlar[max] <= Puanlar[i])
+                {
+                    max = i;
+                }
+            }
+
+
+            for (int i = 0; i < temp.Count; i++)
+            {
+                richTextBox3.Text += Desifrele(temp[i].ToString(), ab[max][0], ab[max][1]) + " ";
+            }
+           label1.Text = "\na=" + ab[max][0].ToString() + " b=" + ab[max][1];
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+
+        }
+
+        void AdeğerleriniUret()
+        {
+            Alar.Clear();
+            Alar.Add(1);
+            for (int i = 1; i < alfabe.Count + 1; i++)
+            {
+                for (int j = 2; j < i + 1; j++)
+                {
+                    if (alfabe.Count % i == 0)
+                        break;
+                    if (i % j == 0 && alfabe.Count % j == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (j == i)
+                            Alar.Add(i);
+
+                    }
+                }
+
+            }
+        }
+
+    }
+}
